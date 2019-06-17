b7352d063248ebaf4d554fa461ccdb5050d5c2a9
David <DavidO256@users.noreply.github.com>
2019-06-17 13:23:21 -0400

    Removed usage of "null" and added fix to filter out empty leaves.

--- src/comments/CommentTree.scala
+++ src/comments/CommentTree.scala
@@ -1,14 +1,13 @@
 package comments
 
 import scala.collection.mutable
-import scala.collection.mutable.Map
 
 class CommentTree(pmid: String) {
   var leaves: List[CommentTree] = List[CommentTree]()
-  var title: String = ""
+  var title: Option[String] = None
 
   def getPMID: String = pmid
-  def getTitle: String = title
+  def getTitle: Option[String] = title
 
   def addLeaf(pmid: String): CommentTree = {
     val leaf = new CommentTree(pmid)
@@ -20,14 +19,13 @@ class CommentTree(pmid: String) {
     def collect(tree: CommentTree, titles: mutable.Map[Int, Set[String]], depth: Int): mutable.Map[Int, Set[String]] = {
       if (!titles.contains(depth))
         titles += (depth -> Set[String]())
-      if (tree.title != "")
-        titles(depth) += tree.title
+      if (tree.title.isDefined)
+        titles(depth) += tree.title.get
       tree.leaves.foreach(leaf => collect(leaf, titles, depth + 1))
       titles
     }
-    collect(this, mutable.Map[Int, Set[String]](), 0)
+    collect(this, mutable.Map[Int, Set[String]](), 0).filter(_._2.nonEmpty)
   }
-
 
   def last_title(): Set[String] = {
     val titles = collectTitles()
